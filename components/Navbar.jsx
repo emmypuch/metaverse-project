2b3818e6b39b75b578f086bf12103e53618ac6db
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2023-08-20 11:47:13 +0100

    wip

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,13 +6,18 @@ import styles from '../styles';
 import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
+  // eslint-disable-next-line no-trailing-spaces
   <motion.nav 
     variants={navVariants}
     initial= "hidden"
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    navbar
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" />
+      <h2 className="font-extabold text-[24px] leading-[30px] text-white">METAVERSUS</h2>
+    </div>
   </motion.nav>
 );
 
